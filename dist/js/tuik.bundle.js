af32460c8c173aa00ecc351fea4b205e36b489a4
pallard-tempo <p.allard@temposoftware.com>
2017-09-22 09:17:26 -0400

    Removed polyfills (tests for jsfiddle)

--- dist/js/tuik.bundle.js
+++ dist/js/tuik.bundle.js
@@ -2770,43 +2770,6 @@ var Dropdown = function () {
   return Dropdown;
 }(Popper);
 
-var Polyfills = function () {
-  // OBJECT ASSIGN
-  if (typeof Object.assign != 'function') {
-    // Must be writable: true, enumerable: false, configurable: true
-    Object.defineProperty(Object, "assign", {
-      value: function assign(target, varArgs) {
-        // .length of function is 2
-        'use strict';
-
-        if (target == null) {
-          // TypeError if undefined or null
-          throw new TypeError('Cannot convert undefined or null to object');
-        }
-
-        var to = Object(target);
-
-        for (var index = 1; index < arguments.length; index++) {
-          var nextSource = arguments[index];
-
-          if (nextSource != null) {
-            // Skip over if undefined or null
-            for (var nextKey in nextSource) {
-              // Avoid bugs when hasOwnProperty is shadowed
-              if (Object.prototype.hasOwnProperty.call(nextSource, nextKey)) {
-                to[nextKey] = nextSource[nextKey];
-              }
-            }
-          }
-        }
-        return to;
-      },
-      writable: true,
-      configurable: true
-    });
-  }
-}();
-
 var Popover = function () {
   var eventHandlers = new Utils.EventHandlers('tuiPopover');
 
@@ -3044,7 +3007,6 @@ var Popover = function () {
   return Popover;
 }(Popper);
 
-exports.Polyfills = Polyfills;
 exports.Utils = Utils;
 exports.Dropdown = Dropdown;
 exports.Popover = Popover;
